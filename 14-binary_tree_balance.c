1faa75463eb66a8232a3add303c3be249a6d4e09
Ezzud <contact@ezzud.fr>
2024-01-09 05:29:36 -0800

    Fixed 14-balance : Added height function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -20,3 +20,23 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (heightL - heightR);
 }
+
+/**
+* binary_tree_height - measures the height of a binary tree
+* @tree: pointer to the root node of the tree to measure the height
+* Return: height of a binary tree or 0
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	if (left >= right)
+		return (left + 1);
+	else
+		return (right + 1);
+}
