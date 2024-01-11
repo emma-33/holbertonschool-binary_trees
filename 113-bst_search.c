54b7c1d5354fabfa6d7f119e6c7ae4b585a433e8
emma-33 <emma.lateyron@orange.fr>
2024-01-11 11:05:28 +0100

    fix betty 113 bst search

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -11,8 +11,10 @@ bst_t *bst_search(const bst_t *tree, int value)
 		return (NULL);
 
 	if (tree->n == value)
-	    return ((bst_t *)tree);
+	{
+		return ((bst_t *)tree);
 
+	}
 	else if (tree->n > value)
 		return (bst_search(tree->left, value));
 
